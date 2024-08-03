2f21ba021e5ef3067d53830502aaaedbb7faa79e
pipok35 <reactpipok@gmail.com>
2024-08-03 19:05:10 +0500

    fix: Мелкие правки

--- src/views/pages/Video.vue
+++ src/views/pages/Video.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="p-4">
+  <div v-if="video" class="p-4">
+    {{ videoUrl }}
     <h1 class="text-2xl font-bold mb-4">{{ video.title }}</h1>
     <p class="mb-4">{{ video.description }}</p>
     <video :src="videoUrl" controls class="w-full"></video>
@@ -7,23 +8,20 @@
 </template>
 
 <script setup lang="ts">
+import qs from 'qs'
 import { ref, onMounted, computed } from 'vue'
 import { useVideoStore } from '@/stores/videos'
 import { useRoute } from 'vue-router'
 import { useApiUrl } from '@/useApiUrl'
+import { Video } from '@/interfaces/video'
 
 const apiUrl = useApiUrl()
 const route = useRoute()
 const videoStore = useVideoStore()
-const video = ref<{title: string, description: string, url: string, filename: string}>({
-  title: '',
-  description: '',
-  url: '',
-  filename: ''
-})
+const video = ref<Video | null>(null)
 
 const videoUrl = computed(() => {
-  return `${apiUrl}/files/download?url=${encodeURIComponent(video.value.url)}`
+  return video.value ? `${apiUrl}/files/download?${qs.stringify({ url: video.value.url })}` : ''
 })
 
 onMounted(async () => {
